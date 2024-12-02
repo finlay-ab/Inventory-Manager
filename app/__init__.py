dea478214199b7d0918fb73935d1b91daea4a320
finlay brydges <100164471+finlay-ab@users.noreply.github.com>
2024-12-02 16:05:49 +0000

    testing log in

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')  # Loads configuration from config.py
@@ -12,5 +13,14 @@ csrf = CSRFProtect(app)
 # Initialize database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Initialize login management
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 from app import views, models
--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, DateField, BooleanField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Confirm Password')
+    submit = SubmitField('Sign Up')
